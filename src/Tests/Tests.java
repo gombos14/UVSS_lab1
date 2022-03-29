4f0a78b59e58d8c7de805b2bff0214a2917b04f3
DanielLucaci <46228296+DanielLucaci@users.noreply.github.com>
2022-03-29 16:05:54 +0300

    Added tests package

---
+++ src/Tests/Tests.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Tests {
+
+    @Test
+    void generateRandomNumbers() {
+        assertTrue(true);
+    }
+}
